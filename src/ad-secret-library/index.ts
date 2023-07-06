ebf7672c96af6f83271dc1d66004cf7ab0f6fd8c
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-06 08:46:48 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/ad-secret-library/index.ts
+++ src/ad-secret-library/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vault/3.17.0/docs/resources/ad_secret_library
 // generated from terraform resource schema
 
