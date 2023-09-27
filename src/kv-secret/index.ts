66eccb0998386754a419e460df987164ecb47584
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-27 09:57:58 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@96f1a7a

--- src/kv-secret/index.ts
+++ src/kv-secret/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vault/3.20.1/docs/resources/kv_secret
 // generated from terraform resource schema
 
