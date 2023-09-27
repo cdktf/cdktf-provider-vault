482c72db71d33d039b0058d5e46456d4fa8a4a84
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-27 08:46:42 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/approle-auth-backend-role-secret-id/index.ts
+++ src/approle-auth-backend-role-secret-id/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vault/3.20.1/docs/resources/approle_auth_backend_role_secret_id
 // generated from terraform resource schema
 
