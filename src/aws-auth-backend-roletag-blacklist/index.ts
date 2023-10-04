3174239feb198bc36ccd8059df61fa5143a2c0ac
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-04 14:30:33 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@95054db

--- src/aws-auth-backend-roletag-blacklist/index.ts
+++ src/aws-auth-backend-roletag-blacklist/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vault/3.20.1/docs/resources/aws_auth_backend_roletag_blacklist
 // generated from terraform resource schema
 
--- src/data-vault-nomad-access-token/index.ts
+++ src/data-vault-nomad-access-token/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vault/3.20.1/docs/data-sources/nomad_access_token
 // generated from terraform resource schema
 
