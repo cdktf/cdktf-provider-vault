fa9a6f5c5b8f81f25e32f0c5b876f80f972fe358
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-22 14:42:00 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/ad-secret-role/index.ts
+++ src/ad-secret-role/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vault/3.24.0/docs/resources/ad_secret_role
 // generated from terraform resource schema
 
