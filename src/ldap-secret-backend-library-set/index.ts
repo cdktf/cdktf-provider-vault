2cc1dd1b23919141eb9bdfe3fa228330429b00f2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-18 03:56:15 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/ldap-secret-backend-library-set/index.ts
+++ src/ldap-secret-backend-library-set/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/vault/4.3.0/docs/resources/ldap_secret_backend_library_set
 // generated from terraform resource schema
 
