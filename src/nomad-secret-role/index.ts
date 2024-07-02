b55877a2b94424e25918ad961b63f03be07aeabe
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-02 13:57:26 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@95cc103

--- src/nomad-secret-role/index.ts
+++ src/nomad-secret-role/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/vault/4.3.0/docs/resources/nomad_secret_role
 // generated from terraform resource schema
 
